4e8bd593e30d0dbf8258ac56df71cb2e43051b52
Radagast27 <cfitzg1983@gmail.com>
2018-11-09 18:37:08 -0600

    Update R_to_MSSQL.r

--- R_to_MSSQL.r
+++ R_to_MSSQL.r
@@ -1,3 +1,4 @@
 library(RODBC)  
- dbconnection <- odbcDriverConnect('driver={SQL Server};server=.;database=CLARO;trusted_connection=true')  
- initdata <- sqlQuery(dbconnection,paste('SELECT * FROM [CLARO].[dbo].[Fielding];')) 
+dbconnection <- odbcDriverConnect('driver={SQL Server};server=.;database=CLARO;trusted_connection=true')  
+initdata <- sqlQuery(dbconnection,paste('SELECT * FROM [CLARO].[dbo].[Fielding];'))
+initdata
